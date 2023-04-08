85be8140bb5fa7a34351ebc38956cb1c2d2b7f2b
Parkman145 <ParkerLearmonth@gmail.com>
2023-04-08 16:10:54 -0700

    Add some swerve drive code to robot.py

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 import wpilib
 import wpilib.drive
+import wpimath
 import rev
 
 
@@ -13,7 +14,17 @@ class MyRobot(wpilib.TimedRobot):
         self.test_motor = rev.CANSparkMax(5, rev._rev.CANSparkMaxLowLevel.MotorType.kBrushless)
         self.stick = wpilib.Joystick(1)
         self.timer = wpilib.Timer()
-
+        self.swerveLocations = {
+            "frontright":wpimath.geometry.Translation2d(0.3556, -0.3556),
+            "rearright":wpimath.geometry.Translation2d(0.3556, -0.3556),
+            "frontleft":wpimath.geometry.Translation2d(0.3556, -0.3556),
+            "rearleft":wpimath.geometry.Translation2d(0.3556, -0.3556),
+        }
+        self.robotCenter = wpimath.geometry.Translation2d(0.0, 0.0)
+        self.kinematics = wpimath.kinematics.SwerveDrive4Kinematics(self.swerveLocations["frontright"], 
+                                                                    self.swerveLocations["rearright"], 
+                                                                    self.swerveLocations["frontleft"], 
+                                                                    self.swerveLocations["rearleft"])
     def autonomousInit(self):
         """This function is run once each time the robot enters autonomous mode."""
         self.timer.reset()
@@ -32,7 +43,8 @@ class MyRobot(wpilib.TimedRobot):
     def teleopPeriodic(self):
         """This function is called periodically during operator control."""
         self.test_motor.set(self.stick.getY())
-
+        chassisSpeeds = wpimath.kinematics.ChassisSpeeds(self.stick.getX(), self.stick.getY(), 0)
+        fl, fr, bl, br = self.kinematics.toSwerveModuleStates(chassisSpeeds)
 
 if __name__ == "__main__":
     wpilib.run(MyRobot)
