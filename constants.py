2e8176cd61812f93014bc79d3c2e9efb52951cca
Parkman145 <ParkerLearmonth@gmail.com>
2024-01-19 18:02:26 -0800

    make constants.py obey naming scheme

--- constants.py
+++ constants.py
@@ -8,60 +8,60 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 # Get the name of the robot (may be moved to main.py)
-robot_name = environ.get("Robot")
-logger.info("Current Robot %s", robot_name)
+ROBOT_NAME = environ.get("Robot")
+logger.info("Current Robot %s", ROBOT_NAME)
 
 class ControllerConstants:
-    ControllerMainID = 0
-    ControllerAuxID = 1
+    CONTROLLER_MAIN_ID = 0
+    CONTROLLER_AUX_ID = 1
     
 class DrivetrainConstants:
-    kMotorDriveFrontRightID = 26
-    kMotorDriveRearRightID = 1
-    kMotorDriveFrontLeftID = 2
-    kMotorDriveRearLeftID = 3
+    MOTOR_DRIVE_FRONT_RIGHT = 26
+    MOTOR_DRIVE_REAR_RIGHT_ID = 1
+    MOTOR_DRIVE_FRONT_LEFT_ID = 2
+    MOTOR_DRIVE_REAR_LEFT_ID = 3
 
-    kMotorTurnFrontRightID = 4
-    kMotorTurnRearRightID = 5
-    kMotorTurnFrontLeftID = 6
-    kMotorTurnRearLeftID = 7
+    MOTOR_TURN_FRONT_RIGHT_ID = 4
+    MOTOR_TURN_REAR_RIGHT_ID = 5
+    MOTOR_TURN_FRONT_LEFT_ID = 6
+    MOTOR_TURN_REAR_LEFT_ID = 7
 
-    kEncoderTurnFrontRightID = 8
-    kEncoderTurnRearRightID = 9
-    kEncoderTurnFrontLeftID = 10
-    kEncoderTurnRearLeftID = 11
+    ENCODER_TURN_FRONT_LEFT_ID = 8
+    ENCODER_TURN_REAR_LEFT_ID = 9
+    ENCODER_TURN_FRONT_LEFT_ID = 10
+    ENCODER_TURN_REAR_LEFT_ID = 11
 
     class Offsets:
         print(environ.get("Robot"))
             
         # Set the offsets depending on the robot name 
         # Maybe add default Case
-        if robot_name is None:
+        if ROBOT_NAME is None:
             raise Exception("Robot Name Not Set!")
-        if robot_name == "Kava":
-            kFrontRight = [-40.429]
-            kRearRight = [66.181]
-            kFrontLeft = [-94.921]
-            kRearLeft = [-1.142]
+        if ROBOT_NAME == "Kava":
+            FRONT_RIGHT = [-40.429]
+            REAR_RIGHT = [66.181]
+            FRONT_LEFT = [-94.921]
+            REAR_LEFT = [-1.142]
 
-        elif robot_name == "T-Shirt Shooter":
-            kFrontRight = [145.107] 
-            kRearRight = [60.732]
-            kFrontLeft = [39.023]
-            kRearLeft = [2.285]
+        elif ROBOT_NAME == "T-Shirt Shooter":
+            FRONT_RIGHT = [145.107] 
+            REAR_RIGHT = [60.732]
+            FRONT_LEFT = [39.023]
+            REAR_LEFT = [2.285]
         else:
             raise Exception("Robot Name Invalid!")
 
     class Calculations:
-        kFinalDriveRatio= 6.75 * 360 #degrees
-        kFinalTurnRatio = (14.0 / 50.0) * (10.0 / 60.0)
-        kWheelCircumference = pi * 3.8 #inches
+        FINAL_DRIVE_RATIO = 6.75 * 360 #degrees
+        FINAL_TURN_RATIO = (14.0 / 50.0) * (10.0 / 60.0)
+        WHEEL_CIRCUMFERENCE = pi * 3.8 #inches
 
-        kModuleMaxSpeed = 16.3 #Feet Per Second
-        kChassisMaxSpeed = 16.3
+        MODULE_MAX_SPEED = 16.3 #Feet Per Second
+        CHASSIS_MAX_SPEED = 16.3
 
-        kModuleMaxAngularVelocity= pi * 4  #radians per second
-        kModuleMaxAngularAcceleration= pi * 8/1  #radians per second^2
+        MODULE_MAX_ANGULAR_VELOCITY = pi * 4  #radians per second
+        MODULE_MAX_ANGULAR_ACCELERATION = pi * 8/1  #radians per second^2
 
-        kMotorMaxOutput = 0.5
-        kMotorDeadband = 0.1
+        MOTOR_MAX_OUTPUT = 0.5
+        MOTOR_DEADBAND = 0.1
