cc050cb8f30df3edc137128ad27e262167fd6380
Nathan <nkeidel00@gmail.com>
2024-01-29 22:39:28 -0800

    new exception for windows users

--- modules/debugmsgs.py
+++ modules/debugmsgs.py
@@ -1,12 +1,25 @@
 # Colors yay
+import platform
 import colorama
+
+# Get OS name
+osName = platform.system()
 
 # Functions for different output messages (they are colorfull so they are easy to spot)
 def debugMsg(message):
-    print(colorama.Style.BRIGHT + colorama.Fore.CYAN + ('DEBUG: ' + str(message)) + colorama.Fore.WHITE + colorama.Style.NORMAL)
+    if 'windows' in osName.lower():
+        print('DEBUG: ' + str(message))
+    else:
+        print(colorama.Style.BRIGHT + colorama.Fore.CYAN + ('DEBUG: ' + str(message)) + colorama.Fore.WHITE + colorama.Style.NORMAL)
 
 def successMsg(message):
-    print(colorama.Style.BRIGHT + colorama.Fore.GREEN + ('SUCCESS: ' + str(message)) + colorama.Fore.WHITE + colorama.Style.NORMAL)
+    if 'windows' in osName.lower():
+        print('SUCCESS: ' + str(message))
+    else:
+        print(colorama.Style.BRIGHT + colorama.Fore.GREEN + ('SUCCESS: ' + str(message)) + colorama.Fore.WHITE + colorama.Style.NORMAL)
 
 def errorMsg(message, error):
-    print(colorama.Style.BRIGHT + colorama.Fore.RED + ('ERROR: ' + str(message) + '\n\n\t> ' + str(error)) + colorama.Fore.WHITE + colorama.Style.NORMAL)
+    if 'windows' in osName.lower():
+        print('ERROR:' + str(message) + '\n\n\t> ' + str(error))
+    else:
+        print(colorama.Style.BRIGHT + colorama.Fore.RED + ('ERROR: ' + str(message) + '\n\n\t> ' + str(error)) + colorama.Fore.WHITE + colorama.Style.NORMAL)
