c6426dabb41d0651108289d790d449aed1c4d873
Nathan <nkeidel00@gmail.com>
2024-02-06 23:00:35 -0800

    update 'robotconfig.py'

--- modules/robotconfig.py
+++ modules/robotconfig.py
@@ -1,9 +1,3 @@
 # Terminal gui for configuring robot
 # NOTE: Do not run on roboRio, for python configuration only
 import json, subprocess, platform, os
-
-# Load path to
-if 'windows' in platform.platform():
-    data_file = os.getcwd()+'\\constants'
-else:
-    data_file = os.getcwd()+'/constants'
