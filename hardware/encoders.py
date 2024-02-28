52773b2d1e61016de2305ae9ad1b67636ec1efa3
Nathan <nkeidel00@gmail.com>
2024-02-27 19:48:46 -0800

    update 'encoders.py'

---
+++ hardware/encoders.py
@@ -0,0 +1,11 @@
+# Module for controlling different encoders
+from extras.debugmsgs import *
+
+import wpimath.kinematics
+import wpimath.geometry
+import wpimath.controller
+import wpimath.trajectory
+import wpimath.units
+
+import rev, phoenix6
+
